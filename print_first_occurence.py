bb404680c900849f081382b3f68ad62d0ef44bd7
Paliwalvip's24 <56674076+Vipipaliwal24@users.noreply.github.com>
2020-03-17 18:38:36 +0530

    Add files via upload

---
+++ print_first_occurence.py
@@ -0,0 +1,13 @@
+T=int(input())
+for i in range(T):
+    s=input().lower()
+    a=[]
+    for i in s:
+        if i not in a:
+            a.append(i)
+            print(i,end="")
+    print()
+        
+        
+              
+    
